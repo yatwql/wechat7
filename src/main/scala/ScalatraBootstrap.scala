2889105a9fab3fb5c19c2ff23b580e6477a41339
Stallman WANG <Stallman.WANG@gmail.com>
2014-04-02 01:31:18 +0800

    update bootstrap

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -5,5 +5,6 @@ import javax.servlet.ServletContext
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
     context.mount(new MyScalatraServlet, "/*")
+    context.addServlet(classOf[JettyLauncher], "/*")
   }
 }
