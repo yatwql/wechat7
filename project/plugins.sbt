f38d4ae7e67db1047f2811c4374c30293a87595f
Stallman WANG <Stallman.WANG@gmail.com>
2014-04-03 00:16:32 +0800

    02Apr2014

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,3 +4,5 @@ addSbtPlugin("org.scalatra.sbt" % "scalatra-sbt" % "0.3.2")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-start-script" % "0.10.0")
 
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.4.0")
+
--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -2,9 +2,10 @@ import wechat._
 import org.scalatra._
 import javax.servlet.ServletContext
 
+
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
     context.mount(new MyScalatraServlet, "/*")
-    context.addServlet(classOf[JettyLauncher], "/*")
+    context.addServlet("/*",classOf[wechat.MyScalatraServlet] )
   }
 }
