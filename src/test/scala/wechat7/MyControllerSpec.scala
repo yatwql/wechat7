84be464686eb0f33bd943bca7f2ae61cf21a7667
Stallman WANG <Stallman.WANG@gmail.com>
2014-04-28 22:50:07 +0800

    refactor

---
+++ src/test/scala/wechat7/MyControllerSpec.scala
@@ -0,0 +1,16 @@
+package wechat7
+
+import org.scalatra.test.specs2._
+
+// For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
+class MyControllerSpec extends ScalatraSpec { def is =
+  "GET / on MyScalatraServlet"                     ^
+    "should return status 200"                  ! root200^
+                                                end
+
+  addServlet(classOf[MyController], "/*")
+
+  def root200 = get("/") {
+    status must_== 200
+  }
+}
