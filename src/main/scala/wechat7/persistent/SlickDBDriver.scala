a2fa1112fc815b6362f872288b63fe93ad1ddf24
Stallman WANG <Stallman.WANG@gmail.com>
2014-05-01 02:32:24 +0800

    db change

--- src/main/scala/wechat7/persistent/SlickDBDriver.scala
+++ src/main/scala/wechat7/persistent/SlickDBDriver.scala
@@ -9,8 +9,9 @@ object SlickDBDriver {
 val TEST = "test"
 val DEV = "dev"
 val PROD = "prod"
+  
 def getDriver: JdbcProfile = {
-scala.util.Properties.envOrElse("runMode", "prod") match {
+scala.util.Properties.envOrElse("runMode", PROD) match {
 case TEST => H2Driver
 case DEV => PostgresDriver
 case PROD => PostgresDriver
