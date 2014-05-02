2c38dd39f0c05a7913b6e1d367e18ed2b5ea3890
Stallman WANG <Stallman.WANG@gmail.com>
2014-05-03 02:19:09 +0800

    update build.scala

--- project/build.scala
+++ project/build.scala
@@ -29,6 +29,7 @@ object DragonStudioWebchatAppBuild extends Build {
       resolvers += Classpaths.typesafeReleases,
       resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/",
       resolvers += "Sonatype Releases"  at "http://oss.sonatype.org/content/repositories/releases",
+      resolvers += "spray repo" at "http://repo.spray.io",
 
       libraryDependencies ++= Seq(
         "org.scalatra" %% "scalatra" % ScalatraVersion,
@@ -45,6 +46,10 @@ object DragonStudioWebchatAppBuild extends Build {
          "com.typesafe" % "config" % "1.2.0",
         "org.slf4j" % "slf4j-nop" % "1.6.4",
         "com.h2database" % "h2" % "1.3.166",
+        "io.spray" % "spray-caching" % "1.3.1",
+        "io.spray" % "spray-util" % "1.3.1",
+        "com.googlecode.concurrentlinkedhashmap" % "concurrentlinkedhashmap-lru" % "1.4",
+          "com.typesafe.akka" %% "akka-actor" % "2.2.3",
         "junit" % "junit" % "4.8.1" % "test"
       ),
       
