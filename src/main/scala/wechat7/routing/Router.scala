ed12243e1ff29a2e7187a02a0eace533ef39b977
Stallman WANG <Stallman.WANG@gmail.com>
2014-05-03 09:33:44 +0800

    add cache support

--- src/main/scala/wechat7/routing/Router.scala
+++ src/main/scala/wechat7/routing/Router.scala
@@ -1,12 +1,20 @@
 package wechat7.routing
 
 import wechat7.util._
+import scala.concurrent.Future
+import akka.actor.ActorSystem
+import spray.caching.{ LruCache, Cache }
+import spray.util._
+
 import scala.slick.driver.JdbcProfile
 import wechat7.persistent._
 
 import scala.xml._
 class Router extends SlickRepo with AdminRepo with UserRepo with VoteRepo with ArticleRepo {
- import profile.simple._
+  import profile.simple._
+  val system = ActorSystem()
+  import system.dispatcher
+  val nicknames: Cache[Option[String]] = LruCache()
 
   def response(requestXml: Option[Elem]): Node = {
     val appUserId = (requestXml.get \ "ToUserName").text
@@ -17,27 +25,35 @@ class Router extends SlickRepo with AdminRepo with UserRepo with VoteRepo with A
     println("Get Message Type  " + msgType + " from user " + openId)
     audit(openId, appUserId, msgType, requestXmlContent)
     val responseXml = responseImpl(openId, appUserId, msgType, requestXml, requestContent)
-    val responseContent=responseXml.toString()
-    val responseMsgType=(responseXml \\ "MsgType").text
+    val responseContent = responseXml.toString()
+    val responseMsgType = (responseXml \\ "MsgType").text
     audit(appUserId, openId, responseMsgType, responseContent)
     responseXml
   }
 
-  def responseImpl(openId: String, appUserId: String, msgType: String, requestXml:  Option[Elem], requestContent: String): Node = {
+  def responseImpl(openId: String, appUserId: String, msgType: String, requestXml: Option[Elem], requestContent: String): Node = {
     val responseContent = " Thanks for your information '" + requestContent + "' with msg type " + msgType
     WechatUtils.getTextMsg(appUserId, openId, responseContent);
   }
-  
-  override def getNickname(openId:String):Option[String]={
-    val s=super.getNickname(openId)
-    val nickname=s match {
+
+  def getNicknameFromDB(openId: String): Option[String] = {
+    println(" Visit DB to get nickname for openid "+openId)
+    val s = super.getNickname(openId)
+    val nickname = s match {
       case Some(t) => t
       case None => {
-         addUser(WechatUtils.getUserInfo(openId))
+        addUser(WechatUtils.getUserInfo(openId))
       }
       case _ => "not found"
     }
     Some(nickname)
+  }
+
+  override def getNickname(openId: String): Option[String] = {
+     println(" Get nickname for openid "+openId)
+    nicknames(openId) {
+      getNicknameFromDB(openId)
+    }.await()
   }
 }
 
