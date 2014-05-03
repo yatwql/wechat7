c2772d187ee8f46b3f12da7910706342efbcffc5
Stallman WANG <Stallman.WANG@gmail.com>
2014-05-03 22:45:16 +0800

    haha

--- src/main/scala/wechat7/persistent/SlickRepo.scala
+++ src/main/scala/wechat7/persistent/SlickRepo.scala
@@ -118,11 +118,10 @@ class SlickRepo(override val profile: JdbcProfile = SlickDBDriver.getDriver) ext
       accounts.list foreach println
       voteTopics.insert(VoteTopic("redwine", "Favour contry"))
 
-      articles.insert(Article("New Title 1", "Description", "1", "text"))
+      articles.insert(Article("题目1", "Description", "1", "text"))
       articles.insert(Article("New Title 2A", "New Description 2A", "2", "news", Constants.REDWINE_PIC, Constants.SHOP_AT_DIANPING))
       articles.insert(Article("New Title 2B", "New Description 2B", "2", "news", Constants.REDWINE_PIC, Constants.SHOP_AT_DIANPING))
       articles.insert(Article("New Title 3", "New Description", "3", "news", Constants.REDWINE_PIC, Constants.SHOP_AT_DIANPING))
-      articles.insert(Article("Helps", "This is for helping", "?", "news", Constants.REDWINE_PIC, Constants.SHOP_AT_DIANPING))
       articles.insert(Article("Helps", "This is for helping", "help", "news", Constants.REDWINE_PIC, Constants.SHOP_AT_DIANPING))
       "population OK"
     }
