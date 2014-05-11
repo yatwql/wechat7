abf81096db3d0c543cf5f3089fdee0a8149ac805
Stallman WANG <Stallman.WANG@gmail.com>
2014-05-12 03:13:02 +0800

    update mode

--- src/main/scala/wechat7/repo/DBConnection.scala
+++ src/main/scala/wechat7/repo/DBConnection.scala
@@ -6,7 +6,7 @@ import com.typesafe.config.ConfigFactory
 class DBConnection(override val profile: JdbcProfile) extends Profile {
 import profile.simple._
 def dbObject(): Database = {
-val env = scala.util.Properties.envOrElse("runMode", "dev")
+val env = scala.util.Properties.envOrElse("runMode", "prod")
 val config = ConfigFactory.load(env)
 val url = config.getString("db.url")
 val username = config.getString("db.username")
