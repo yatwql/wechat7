e7f6dad0b497bb2e621cbc64ad02343880563704
Stallman WANG <Stallman.WANG@gmail.com>
2014-05-14 01:11:36 +0800

    update cache mgr

--- src/main/scala/wechat7/util/CacheMgr.scala
+++ src/main/scala/wechat7/util/CacheMgr.scala
@@ -4,11 +4,12 @@ import scala.xml.Node
 
 import spray.caching.Cache
 import spray.caching.LruCache
+import scala.concurrent.duration._
 object CacheMgr {
-  val nicknameCache: Cache[Option[String]] = LruCache(maxCapacity = 500)
-  val userActionCache: Cache[Option[String]] = LruCache(maxCapacity = 500)
-  val nextActionCache: Cache[Option[String]] = LruCache(maxCapacity = 100)
-  val currentActionCache: Cache[Option[String]] = LruCache(maxCapacity = 100)
-  val articleCache: Cache[Seq[Node]] = LruCache(maxCapacity = 100)
-  val voteThreadCache: Cache[Option[(String, String, Int, Seq[String])]] = LruCache(maxCapacity = 100)
+  val nicknameCache: Cache[Option[String]] = LruCache(maxCapacity = 500,initialCapacity=100,timeToLive=Duration(24,HOURS),timeToIdle=Duration(12,HOURS))
+  val userActionCache: Cache[Option[String]] = LruCache(maxCapacity = 500,initialCapacity=100,timeToLive=Duration(30,MINUTES),timeToIdle=Duration(20,MINUTES))
+  val nextActionCache: Cache[Option[String]] = LruCache(maxCapacity = 64,initialCapacity=32,timeToLive=Duration(2,HOURS),timeToIdle=Duration(1,HOURS))
+  val currentActionCache: Cache[Option[String]] = LruCache(maxCapacity = 64,initialCapacity=32,timeToLive=Duration(2,HOURS),timeToIdle=Duration(1,HOURS))
+  val articleCache: Cache[Seq[Node]] = LruCache(maxCapacity = 64,initialCapacity=32,timeToLive=Duration(2,HOURS),timeToIdle=Duration(1,HOURS))
+  val voteThreadCache: Cache[Option[(String, String, Int, Seq[String])]] = LruCache(maxCapacity = 64,initialCapacity=32,timeToLive=Duration(2,HOURS),timeToIdle=Duration(1,HOURS))
 }
